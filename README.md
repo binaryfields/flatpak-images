2f7f950c882715e7d6d1d82ca31fba6a4968c031
raytracer <raytracer@skyline.origin>
2018-02-20 23:49:14 -0500

    images: add initial set of apps and extensions

---
+++ README.md
@@ -0,0 +1,10 @@
+# Flatpak Images
+
+## Extensions
+
+		flatpak-builder --repo=/home/raytracer/.var/repo/dsio --force-clean build/openjdk8 org.freedesktop.Sdk.Extension.openjdk8/org.freedesktop.Sdk.Extension.openjdk8.json
+
+## Apps
+
+		flatpak-builder --repo=/home/raytracer/.var/repo/dsio --force-clean build/idea com.jetbrains.IntelliJIDEA/com.jetbrains.IntelliJIDEA.json
+
---
+++ apps.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+repo=~/.var/repo/dsio
+build_dir=build
+gpg_dir=~/.gnupg
+gpg_key=596AE2EE4A73DB1605334629662603BD42FFB34C
+
+declare -a apps=(
+	"com.google.AndroidStudio"
+	"com.jetbrains.CLion"
+	"com.jetbrains.IntelliJIDEA"
+	"com.sublimetext.three"
+)
+
+
+mkdir -p "${build_dir}"
+
+for app in "${apps[@]}"; do
+	if [ -e "${app}/${app}.json" ]; then
+		flatpak-builder --force-clean --gpg-homedir=${gpg_dir} --gpg-sign=${gpg_key} --repo=${repo} ${build_dir}/${app} ${app}/${app}.json
+	fi
+done
+flatpak build-update-repo --generate-static-deltas --gpg-homedir=${gpg_dir} --gpg-sign=${gpg_key} ${repo}
