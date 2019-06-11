07b0a3c748f56f60663dcd758dd90d355f726414
Sebastian Jastrzebski <sebby2k@gmail.com>
2019-06-10 20:02:24 -0400

    update dev ides

--- apps.sh
+++ apps.sh
@@ -6,13 +6,13 @@ gpg_dir=gpg
 gpg_key=61631B1ADEDD6AC12ECFE71E0BC5AF3C03677912
 
 declare -a apps=(
-	#"com.google.AndroidStudio"
-	#"com.jetbrains.CLion"
-	#"com.jetbrains.IntelliJIDEA"
-	#"com.sublimetext.three"
-	#"com.visualstudio.VSCode"
+	"com.google.AndroidStudio"
+	"com.jetbrains.CLion"
+	"com.jetbrains.IntelliJIDEA"
+	"com.sublimetext.three"
+	"com.visualstudio.VSCode"
 	"net.fsuae.FS-UAE"
-	#"net.sf.VICE"
+	"net.sf.VICE"
 )
 
 mkdir -p "${build_dir}"
