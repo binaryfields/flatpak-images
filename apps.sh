eb82df3e33df1489d6d65102dde88ceed54b8ba6
Sebastian Jastrzebski <sebby2k@gmail.com>
2019-08-08 10:11:18 -0400

    upgrade android studio

--- apps.sh
+++ apps.sh
@@ -6,9 +6,9 @@ gpg_dir=gpg
 gpg_key=61631B1ADEDD6AC12ECFE71E0BC5AF3C03677912
 
 declare -a apps=(
-	#"com.google.AndroidStudio"
+	"com.google.AndroidStudio"
 	#"com.jetbrains.CLion"
-	"com.jetbrains.IntelliJIDEA"
+	#"com.jetbrains.IntelliJIDEA"
 	#"com.sublimetext.three"
 	#"com.visualstudio.VSCode"
 	#"net.fsuae.FS-UAE"
