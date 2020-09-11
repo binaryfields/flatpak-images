fdbeab712036d16b5b66c852ca7a7a6e5965557b
Sebastian Jastrzebski <sebby2k@gmail.com>
2020-09-11 00:26:48 -0400

    clion: update to 2020.2

--- apps.sh
+++ apps.sh
@@ -1,27 +1,25 @@
 #!/usr/bin/env bash
 
-repo=~/.var/repo/dsio
 build_dir=build
-gpg_dir=gpg
-gpg_key=61631B1ADEDD6AC12ECFE71E0BC5AF3C03677912
+repo_dir=repo
 
 declare -a apps=(
 	#"com.google.AndroidStudio"
-	#"com.jetbrains.CLion"
-	"com.jetbrains.IntelliJIDEA"
+	"com.jetbrains.CLion"
+	#"com.jetbrains.IntelliJIDEA"
 	#"net.sf.jasperstudio"
 	#"net.fsuae.FS-UAE"
 	#"net.sf.VICE"
 )
 
-mkdir -p "${build_dir}"
+mkdir -p "${build_dir}" "${repo_dir}"
 
 for app in "${apps[@]}"; do
 	if [ -e "${app}/${app}.yml" ]; then
-		flatpak-builder --repo=${repo} --force-clean ${build_dir}/${app} ${app}/${app}.yml
+		flatpak-builder --repo=${repo_dir} --force-clean ${build_dir}/${app} ${app}/${app}.yml
 	fi
 done
-flatpak build-sign ${repo} --gpg-sign=${gpg_key} --gpg-homedir=${gpg_dir}
-flatpak build-update-repo ${repo} --gpg-sign=${gpg_key} --gpg-homedir=${gpg_dir}
 
+#flatpak build-sign ${repo} --gpg-sign=${gpg_key} --gpg-homedir=${gpg_dir}
+#flatpak build-update-repo ${repo} --gpg-sign=${gpg_key} --gpg-homedir=${gpg_dir}
 #flatpak build-update-repo --generate-static-deltas --gpg-homedir=${gpg_dir} --gpg-sign=${gpg_key} ${repo}
---
+++ repo.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+flatpak remote-add --user --no-gpg-verify dsio repo
